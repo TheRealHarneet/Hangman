23e4d3fa9357736bdee545a6da7133233d586d03
TheRealHarneet <68132395+TheRealHarneet@users.noreply.github.com>
2021-02-07 14:48:44 -0500

    Update Hangman.py

--- Hangman.py
+++ Hangman.py
@@ -62,13 +62,10 @@ word_list = ["ardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 word_length = len(chosen_word)
 
-# TODO-1: - Create a variable called 'lives' to keep track of the number of lives left.
-# Set 'lives' to equal 6.
 
 player_lives = 6
 
-# Testing code
-print(f'Pssst, the solution is {chosen_word}.')
+
 
 # Create blanks
 display = []
@@ -87,10 +84,6 @@ while not end_of_game:
 
     print(display)
 
-    # TODO-2: - If guess is not a letter in the chosen_word,
-    # Then reduce 'lives' by 1.
-    # If lives goes down to 0 then the game should stop and it should print "You lose."
-    # Join all the elements in the list and turn it into a String.
 
     if guess not in chosen_word:
         player_lives -= 1
@@ -106,6 +99,6 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    # TODO-3: - print the ASCII art from 'stages' that corresponds to the current number of 'lives' the user has remaining.
+    
     print(stages[player_lives])
 
